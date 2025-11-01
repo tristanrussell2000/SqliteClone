77e5c3ec0038d4442feda3ac62226860e3822501
Tristan Russell <tristanrussell2000@gmail.com>
2025-11-01 14:20:41 -0700

    Chapter 6 - Convert to a cursor abstraction

--- main.c
+++ main.c
@@ -91,6 +91,38 @@ typedef struct {
 	Pager* pager;
 } Table;
 
+typedef struct {
+	Table* table;
+	uint32_t row_num;
+	bool end_of_table;
+} Cursor;
+
+Cursor* table_start(Table* table) {
+	Cursor* cursor = malloc(sizeof(Cursor));
+	cursor->table = table;
+	cursor->row_num = 0;
+	cursor->end_of_table = (table->num_rows == 0);
+
+	return cursor;
+}
+
+Cursor* table_end(Table* table) {
+	Cursor* cursor = malloc(sizeof(Cursor));
+	cursor->table = table;
+	cursor->row_num = table->num_rows;
+	cursor->end_of_table = true;
+
+	return cursor;
+}
+
+
+void cursor_advance(Cursor* cursor) {
+	cursor->row_num += 1;
+	if (cursor->row_num >= cursor->table->num_rows) {
+		cursor->end_of_table = true;
+	}
+}
+
 Pager* pager_open(const char* filename) {
 	int fd = open(filename, O_RDWR | O_CREAT, S_IWUSR | S_IRUSR);
 	if (fd == -1) {
@@ -138,6 +170,15 @@ void* get_page(Pager* pager, uint32_t page_num) {
 		pager->pages[page_num] = page;
 	}
 	return pager->pages[page_num];
+}
+
+void* cursor_value(Cursor* cursor) {
+   uint32_t row_num = cursor->row_num;
+   uint32_t page_num = row_num / ROWS_PER_PAGE;
+   void *page = get_page(cursor->table->pager, page_num);
+   uint32_t row_offset = row_num % ROWS_PER_PAGE;
+   uint32_t byte_offset = row_offset * ROW_SIZE;
+   return page + byte_offset;
 }
 
 void pager_flush(Pager* pager, uint32_t page_num, uint32_t size) {
@@ -279,18 +320,22 @@ ExecuteResult execute_insert(Statement* statement, Table* table) {
 	}
 
 	Row* row_to_insert = &(statement->row_to_insert);
+	Cursor* cursor = table_end(table);
 
-	serialize_row(row_to_insert, row_slot(table, table->num_rows));
+	serialize_row(row_to_insert, cursor_value(cursor));
 	table->num_rows += 1;
 	return EXECUTE_SUCCESS;
 }
 
 ExecuteResult execute_select(Statement* statement, Table* table) {
+	Cursor* cursor = table_start(table);
 	Row row;
-	for (uint32_t i = 0; i < table->num_rows; i++) {
-		deserialize_row(row_slot(table, i), &row);
+	while (!(cursor->end_of_table)) {
+		deserialize_row(cursor_value(cursor), &row);
 		print_row(&row);
+		cursor_advance(cursor);
 	}
+	free(cursor);
 	return EXECUTE_SUCCESS;
 }
 
